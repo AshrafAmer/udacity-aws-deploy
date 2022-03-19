4f907f760dd1fbb15531066d2f376723f476ff0e
Ashraf Amer <ashraf.amer55@gmail.com>
2022-03-19 05:20:26 +0200

    upload screenshots

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://udacityapi-env.eba-dvz3ytwr.us-east-1.elasticbeanstalk.com/api/v0",
+  apiHost: process.env.API_HOST,
 };
 
 /*
--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udacityapi-env.eba-dvz3ytwr.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: process.env.API_HOST,
 };
 
 /*
